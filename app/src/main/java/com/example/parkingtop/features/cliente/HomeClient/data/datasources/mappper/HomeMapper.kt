08320b967f95b0b374e3687498b710d2c5c67717
yuzuokogi18 <233351@ids.upchiapas.edu.mx>
2026-03-27 01:46:23 -0600

    conexion login register

---
+++ app/src/main/java/com/example/parkingtop/features/cliente/HomeClient/data/datasources/mappper/HomeMapper.kt
@@ -0,0 +1,28 @@
+package com.example.parkingtop.features.cliente.HomeClient.data.datasources.mappper
+
+import com.example.parkingtop.features.cliente.HomeClient.data.datasources.models.ParkingLotDTO
+import com.example.parkingtop.features.cliente.HomeClient.data.datasources.models.UserDTO
+import com.example.parkingtop.features.cliente.HomeClient.domain.entities.ParkingLot
+import com.example.parkingtop.features.cliente.HomeClient.domain.entities.UserProfile
+
+fun ParkingLotDTO.toDomain(): ParkingLot {
+    return ParkingLot(
+        id = this.id,
+        name = this.name,
+        address = this.address,
+        availableSpots = this.availableSpots,
+        basePricePerHour = this.basePricePerHour,
+        ratingAverage = this.ratingAverage,
+        imageUrl = this.images.firstOrNull(),
+        latitude = this.latitude.toDoubleOrNull() ?: 0.0,
+        longitude = this.longitude.toDoubleOrNull() ?: 0.0
+    )
+}
+
+fun UserDTO.toDomain(): UserProfile {
+    return UserProfile(
+        id = this.id,
+        fullName = this.fullName,
+        profileImageUrl = this.profileImage
+    )
+}
